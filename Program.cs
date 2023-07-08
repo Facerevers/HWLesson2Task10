96f4296d8ac84ea86dea99ec6757b46145c64b8f
crazle <crazle@mail.ru>
2023-07-08 14:26:18 +0300

    Вывод второй цифры трёхзначного числа

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число и нажмите Enter");
+int number = int.Parse(Console.ReadLine());
+int secondDigit = (number % 100) / 10;
+Console.WriteLine($"{number} -> {secondDigit}");
